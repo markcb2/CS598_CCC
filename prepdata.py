620dcf669ef9d878fe62a41d54d0318784ab663c
Amazing Grace <18647491+John3-16@users.noreply.github.com>
2019-01-26 10:07:11 -0500

    Create prepdata.py
    will unzip the .csv file and remove the zip file leaving only the csv files

---
+++ prepdata.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+import zipfile
+import os
+
+pth = Path(r'D:\GitRepro\UIUC\CS598Capstone\raw_data\s3\aviation\airline_ontime')
+dirs = [x for x in pth.iterdir() if x.is_dir()]
+
+for d in dirs:
+    zips = list(d.glob('*.zip'))
+    if zips == []:
+        print(str(d) + ' path is already unzipped')
+        continue
+
+    os.chdir(d)
+    for z in zips:
+        with zipfile.ZipFile(z) as myzip:
+            for m in myzip.namelist():
+                f = Path(m)
+                if f.suffix == '.csv':
+                    myzip.extract(f.name)
+                break
+        os.remove(z)
+
+    print(str(d) + ' has been unzipped')
