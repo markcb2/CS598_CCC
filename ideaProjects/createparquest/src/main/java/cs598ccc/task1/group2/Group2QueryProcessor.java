38837128459f08f0e31a7c2a4e4630876f0f1d82
markcberman <markcberman@yahoo.com>
2019-02-12 20:23:56 -0600

    added query 2.2

--- ideaProjects/createparquest/src/main/java/cs598ccc/task1/group2/Group2QueryProcessor.java
+++ ideaProjects/createparquest/src/main/java/cs598ccc/task1/group2/Group2QueryProcessor.java
@@ -55,10 +55,20 @@ public class Group2QueryProcessor {
                 .mode("overwrite")
                 .saveAsTable("orign_airports");
 
+
+        Dataset<Row> dest_airports_df = spark.read().format("parquet").load("/tmp/cs598ccc/parquet_data/dest_airports");
+        logger.info("Number of Distinct Destination Airports: " + dest_airports_df.count());
+
+        dest_airports_df.write()
+                .format("parquet")
+                .mode("overwrite")
+                .saveAsTable("dest_airports");
+
         logger.info("Starting query 2.1");
 
         spark.catalog().listTables().show();
 
+        /*
 
         Dataset<Row> query2_1_unfiltered_results_df = spark.sql("SELECT Origin, Carrier, avg(DepDelay) as avgDepartureDelay FROM enriched_ontimeperf WHERE Origin in (SELECT origin_airport_code FROM orign_airports) GROUP By Origin, Carrier  ORDER BY Origin ASC, avgDepartureDelay ASC");
         //query2_1_unfiltered_results_df.show(200);
@@ -88,6 +98,42 @@ public class Group2QueryProcessor {
                 .save("/tmp/cs598ccc/queryResults/group2Dot1_filtered");
 
         logger.info("Finished Group 2: Question 1");
+
+        */
+
+        logger.info("Starting query 2.2");
+
+
+        Dataset<Row> query2_2_unfiltered_results_df = spark.sql("SELECT Origin, Dest, avg(DepDelay) as avgDepartureDelay FROM enriched_ontimeperf WHERE Origin in (SELECT dest_airport_code FROM dest_airports) GROUP By Origin, Dest  ORDER BY Origin ASC, avgDepartureDelay ASC");
+        query2_2_unfiltered_results_df.show(200);
+
+
+        logger.info("Saving query 2.2 unfiltered results as parquet file with  " + query2_2_unfiltered_results_df.count() + " rows ");
+
+        query2_2_unfiltered_results_df.write()
+                .format("parquet")
+                .mode("overwrite")
+                .partitionBy("Origin")
+                .save("/tmp/cs598ccc/queryResults/group2Dot2_unfiltered");
+
+        WindowSpec windowSpec_2_2 = Window.partitionBy("Origin").orderBy(asc("avgDepartureDelay"));
+
+        Dataset<Row> query2_2_filtered_df = query2_2_unfiltered_results_df.withColumn("rank", rank().over(windowSpec_2_2))
+                .where(col("rank").lt(11))
+                .drop("rank");
+
+        query2_2_filtered_df.show(200);
+
+        logger.info("Saving query 2.2 filtered results as parquet file with  " + query2_2_filtered_df.count() + " rows ");
+
+        query2_2_filtered_df.write()
+                .format("parquet")
+                .mode("overwrite")
+                .partitionBy("Origin")
+                .save("/tmp/cs598ccc/queryResults/group2Dot2_filtered");
+
+        logger.info("Finished Group 2: Question 2");
+
 
     }
 }
